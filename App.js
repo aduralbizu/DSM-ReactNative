ca9ee4bffa321a9e34400f3b730ab79013b5fdfe
Adur <adur.albizu@gmail.com>
2024-04-15 16:54:31 +0200

    commitDePrueba

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eff',
     alignItems: 'center',
     justifyContent: 'center',
   },
